f97c43d49f8c83114cac751ba0070d725b7973aa
dikshita18 <60962844+dikshita18@users.noreply.github.com>
2020-09-17 17:02:28 +0530

    Update Practical_5.py

--- Practical_5.py
+++ Practical_5.py
@@ -1,5 +1,7 @@
 #Program to search an element in the list using Linear and Binary search
+
 list_values = [2, 4, 6, 8, 10]
+
 def binary_search(list_values, search, start, end):
     if start > end:
         return -1
@@ -18,8 +20,10 @@ def binary_search(list_values, search, start, end):
 def linear_search(list_values, search):
     index_counter = 0
     list_size = len(list_values)
+
     while index_counter < list_size:
         temp_value = list_values[index_counter]
+
         if temp_value == search:
             return index_counter
         index_counter += 1
@@ -28,8 +32,10 @@ def linear_search(list_values, search):
 
 #Taking input from the user
 ip = int(input("Enter 1 for binary search OR \nEnter 2 for linear search on list:"))
+
 if ip == 1:
     print(binary_search(list_values, 6, 0, len(list_values)))
+
 else:
     print(linear_search(list_values, 6))
 
