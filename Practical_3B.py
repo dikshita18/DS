2739ca5166156a6aaac72a60713df6c1fccacde9
dikshita18 <60962844+dikshita18@users.noreply.github.com>
2020-10-09 09:45:09 +0530

    Update Practical_3B.py

--- Practical_3B.py
+++ Practical_3B.py
@@ -6,9 +6,9 @@ using_rod = 'B'
 
 def hanoi(n, from_rod, to_rod, using_rod):
     if n > 0:
-        hanoi(n-1, from_rod, using_rod, to_rod)
+        hanoi(n - 1, from_rod, using_rod, to_rod)
         print('Move disk from ', from_rod, ' to ', to_rod)
-        hanoi(n-1, using_rod, to_rod, from_rod)
+        hanoi(n - 1, using_rod, to_rod, from_rod)
 
 disks = int(input("Enter number of disks: "))
 hanoi(disks, from_rod, to_rod, using_rod)
